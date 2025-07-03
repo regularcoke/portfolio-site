f3c741766848706199a41d3a5dd98bc6e55b6d9a
regularcoke <150773486+regularcoke@users.noreply.github.com>
2025-07-02 21:44:23 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const blogEntries = [
   {
     date: "2025-07-02",
     title: "caracol",
-    content: "hoy tengo mi vuelo a los angeles..<br>estos ultimos semenas fueron los dias mas dificiles, pero aun estoy aqui, inspirado",
+    content: "hoy tengo mi vuelo a los angeles..<br>estas últimas semanas fueron los días más difíciles, pero aún estoy aquí, inspirado.",
     images:["img/blog/IMG_6250.jpeg",]
   },
   
