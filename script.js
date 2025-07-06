d8972a7ab3ff477d0fbf61fd3e9c5bfe29141ee9
regularcoke <150773486+regularcoke@users.noreply.github.com>
2025-07-06 11:07:19 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 const blogEntries = [
   {
     date: "2025-07-06",
-    title: "i need new life goals",
-    content: "i want to publish a book",
+    title: "me in 5 years",
+    content: "i didnt make an entry yesterday. I wasnt feeling good. <br> but i know now that i want to publish a book. I have to develop new life goals now that i dropped my other ones.<br>i want to be a published artist in 4 years. <br> i want to be a published author in 5 years. <br> i want to be a published designer in 6 years.",
     images:[]
   },
   {
